9fbfbb9a0e4e6c46d3292874ed26c9031121f9d2
EC2 Default User <ec2-user@ip-172-31-16-34.ec2.internal>
2025-11-10 10:01:45 +0000

    3rd update in code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 <code>
 x = 5;
 y = 6;
@@ -8,3 +9,9 @@ num1 = 10
 num2 = 25
 sum_result = num1 + num2
 print(f"The sum of {num1} and {num2} is: {sum_result}")
+number = int(input("Enter an integer: "))
+
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
