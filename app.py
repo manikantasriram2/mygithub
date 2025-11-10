b611d04cb0c7b5d09b80e2d3e4af5148da6140e2
EC2 Default User <ec2-user@ip-172-31-16-34.ec2.internal>
2025-11-10 10:37:24 +0000

    5th code updated

--- app.py
+++ app.py
@@ -15,9 +15,16 @@ if number % 2 == 0:
     print(f"{number} is an even number.")
 else:
     print(f"{number} is an odd number.")
+<<<<<<< Updated upstream
 def greet(name):
     """This function prints a greeting message."""
     print(f"Hello, {name}!")
 
 greet("Alice")
 greet("Bob")
+=======
+    fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(f"I like {fruit}.")
+>>>>>>> Stashed changes
